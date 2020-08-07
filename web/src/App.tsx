0c0df807530b2fb2898d924a4166d4ab153b7c80
hedenica <hedenica@hotmail.com>
2020-08-07 15:56:57 -0300

    ✨ adding landing page to app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Landing from './pages/Landing';
+
+import './assets/styles/global.css';
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Landing />
   );
 }
 
